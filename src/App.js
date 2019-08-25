446dc56acab43121c562afe9542cb5594f5dec05
Leisan <allarepossible@gmail.com>
2019-08-25 21:06:19 +0300

    add some fix

--- src/App.js
+++ src/App.js
@@ -19,15 +19,16 @@ for (let i = 0; i < 20; i++) {
     data.push({color: i, key: i, data: series, opacity: 0.8});
 }
 
+const normalizePrice = price => String(price).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ');
+
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            fullPrice: 0,
-            percent: 0,
-            initialFee: 0,
-            years: 0,
-
+            fullPrice: 6000000,
+            percent: 9.9,
+            initialFee: 2500000,
+            years: 15,
 
             payment: 0,
             credit: 0,
@@ -36,96 +37,77 @@ class App extends Component {
             coef: 0,
             overpayment: 0,
         };
+
         this.handleChangeFullPrice = this.handleChangeFullPrice.bind(this);
         this.handleChangePercent = this.handleChangePercent.bind(this);
         this.handleChangeInitialFee = this.handleChangeInitialFee.bind(this);
         this.handleChangeYears = this.handleChangeYears.bind(this);
     }
+    componentDidMount() {
+        this.setValue(this.state);
+    }
 
-    handleChangeFullPrice(event) {
-        const newFullPrice = event.target.value;
-        const initial = this.state.initialFee;
-        const credit = newFullPrice - initial;
+    setValue(state) {
+        const {fullPrice, initialFee, percent, years} = state;
+        const credit = fullPrice - initialFee;
 
-        const duration = this.state.years * 12;
-        const dem = this.state.percent/1200;
+        const duration = years * 12;
+        const dem = 1 + percent/1200;
 
         const pow = Math.pow(dem, duration);
         const coef = pow * (dem - 1) / (pow - 1);
-        const payment = coef * credit;
 
-        const overpayment = payment * duration - credit;
+        const payment = Math.round(coef * credit * 100) / 100;
+
+        const overpayment = Math.round(payment * duration - credit);
 
         this.setState({
-            fullPrice: newFullPrice,
             payment,
             credit,
             duration,
             dem,
             coef,
             overpayment,
+        });
+    }
+    handleChangeFullPrice(event) {
+        const newFullPrice = event.target.value;
+
+        this.setState(prevState => {
+            const newState = prevState;
+            newState.fullPrice = newFullPrice;
+
+            this.setValue(newState);
         });
     }
     handleChangePercent(event) {
         const newPercent = event.target.value;
-        const duration = this.state.years * 12;
-        const dem = 1 + newPercent/1200;
-        const pow = Math.pow(dem, duration);
-        const coef = pow * (dem - 1) / (pow - 1);
-        const payment = coef * this.state.credit;
 
-        const overpayment = payment * duration - this.state.credit;
+        this.setState(prevState => {
+            const newState = prevState;
+            newState.percent = newPercent;
 
-        this.setState({
-            percent: newPercent,
-            payment,
-            duration,
-            dem,
-            coef,
-            overpayment,
+            this.setValue(newState);
         });
     }
     handleChangeInitialFee(event) {
         const newInitialFee = event.target.value;
-        const credit = this.state.fullPrice - newInitialFee;
-        const dem = 1 + this.state.percent/1200;
-        const duration = this.state.years * 12;
-        const pow = Math.pow(dem, duration);
-        const coef = pow * (dem - 1) / (pow - 1);
-        const payment = coef * credit;
 
-        const overpayment = payment * duration - credit;
+        this.setState(prevState => {
+            const newState = prevState;
+            newState.initialFee = newInitialFee;
 
-        this.setState({
-            initialFee: newInitialFee,
-            payment,
-            credit,
-            duration,
-            dem,
-            coef,
-            overpayment,
+            this.setValue(newState);
         });
     }
     handleChangeYears(event) {
         const newYears = event.target.value;
-        const duration = newYears * 12;
-        const initial = this.state.initialFee;
-        const credit = this.state.fullPrice - initial;
-        const dem = 1 + this.state.percent/1200;
-        const pow = Math.pow(dem, duration);
-        const coef = pow * (dem - 1) / (pow - 1);
-        const payment = coef * credit;
 
-        const overpayment = payment * duration - credit;
+        this.setState(prevState => {
+            const newState = prevState;
+            newState.years = newYears;
 
-        this.setState({
-            years: newYears,
-            payment,
-            credit,
-            duration,
-            dem,
-            coef,
-            overpayment,
+            this.setValue(newState);
         });
     }
 
@@ -177,13 +159,9 @@ class App extends Component {
                         </div>
                     </div>
                     <div className="column">
-                        <span className="label">Ежемесячный платеж {this.state.payment}</span>
-                        <span className="label">Сумма кредита {this.state.credit}</span>
-                        <span className="label">Коэффициент{this.state.coef}</span>
-                        <span className="label">Делитель {this.state.dem}</span>
-                        <span className="label">Всего выплат {this.state.total}</span>
-                        <span className="label">Переплата {this.state.overpayment}</span>
-                        <span className="label">Длительность {this.state.duration}</span>
+                        <span className="label-s">Ежемесячный платеж: {normalizePrice(this.state.payment)}</span>
+                        <span className="label-s">Сумма кредита: {normalizePrice(this.state.credit)}</span>
+                        <span className="label-s">Переплата: {normalizePrice(this.state.overpayment)}</span>
                     </div>
                 </div>
 
