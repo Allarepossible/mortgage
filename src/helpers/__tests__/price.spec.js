d939bebd3f77cf722288c31ad44c0cd2aff8effd
Leisan <allarepossible@gmail.com>
2019-09-12 23:31:21 +0300

    add another test

--- src/helpers/__tests__/price.spec.js
+++ src/helpers/__tests__/price.spec.js
@@ -3,7 +3,10 @@ import {normalizePrice} from '../price';
 describe('Price', () => {
     it('Should be normalized', () => {
         expect(normalizePrice(2020)).toEqual('2 020 ₽');
+    });
 
+    it('Should be normalized', () => {
+        expect(normalizePrice(1010020)).toEqual('1 010 020 ₽');
     });
 });
 
